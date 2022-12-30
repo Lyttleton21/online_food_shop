2a3ba7d26d46238745cd74e4fecf1f7ec623aaf4
Damilare <bolarinwaoluwadamilare91@gmail.com>
2022-12-30 02:28:30 +0100

    remove env.js fro the gitignore file

---
+++ backend/config/env.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "db_host":"localhost",
+    "db_username":"postgres",
+    "db_password":'2001',
+    "db_port":8080,
+    "db_dialect":"postgres",
+    "db_name":"food-mine", // The database name
+}
